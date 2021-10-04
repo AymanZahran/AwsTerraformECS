9aeb0b06cdf468a3ba7c4d8d7bd466e498a60afd
Ayman Zahran <ah.zahran@outlook.com>
2021-10-04 21:04:28 +0200

    Quick-Commit

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "tags" {
+  description = "Tags to propogate to all supported resources"
+  type        = map(string)
+
+  default = {
+    Env        = "ce_express"
+    managed-by = "Terraform"
+  }
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "Name of the VPC being deployed"
+  default     = "My_VPC"
+}
+
+variable "vpc_ip_cidr" {
+  type        = string
+  description = "IP CIDR assigned to the environment"
+  default     = "10.0.0.0/16"
+}

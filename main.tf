fb16d88b20a0c061edb732b7b488235d1596209a
Ayman Zahran <ah.zahran@outlook.com>
2021-10-05 20:17:12 +0200

    Quick-Commit

--- main.tf
+++ main.tf
@@ -1,25 +1,17 @@
-
 module "vpc" {
-  source   = "./modules/aws_vpc/"
-  vpc_name = var.vpc_name
-  vpc_cidr = var.vpc_ip_cidr
-  tags     =  merge(var.tags, {})
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
-
-module "web_server" {
-  source = "./modules/aws_asg_server"
-
-  name                      = var.name
-  vpc_id                    = module.vpc.vpc_id
-  app_private_subnets       = module.vpc.private_subnets
-  volume_size               = var.volume_size
-  tags                      = var.tags
-  region                    = var.region
-  ami                       = "ami-082b5a644766e0e6f"
-  user_data                 = data.template_file.webserver_launch_configuration_user_data.rendered
-}
-
-data "template_file" "webserver_launch_configuration_user_data" {
-  template = file("${path.module}/template/user_data.tpl")
-}
-

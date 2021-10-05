519637ce44938c5ec27c1f387bf4c9046030a696
Ayman Zahran <ah.zahran@outlook.com>
2021-10-05 19:30:01 +0200

    Quick-Commit

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-data "aws_vpc" "vpc" {
-  most_recent = true
-  owners = ["self"]
-  tags = {
-    Name   = "MyVPC"
-  }
-}
 
 module "vpc" {
   source   = "./modules/aws_vpc/"
@@ -19,8 +12,8 @@ module "web_server" {
   source = "./modules/aws_asg_server"
 
   name                      = var.name
-  vpc_id                    = aws_vpc.vpc.outputs.vpc_id
-  app_private_subnets       = aws_vpc.vpc.outputs.private_subnets
+  vpc_id                    = aws_vpc.MyVPC.outputs.vpc_id
+  app_private_subnets       = aws_vpc.MyVPC.outputs.private_subnets
   volume_size               = var.volume_size
   tags                      = var.tags
   region                    = var.region
